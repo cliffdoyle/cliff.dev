acf393babf33fcd80312ff60b139875d34f8f4ae
Cliff Oyoh Omollo <oyoocliff471@gmail.com>
2025-06-23 17:37:24 +0300

    code refactor

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import Socials from './Socials';
+import profilePic from '../assets/profile-pic.jpeg';
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-content">
+      <img src={profilePic} alt="Cliff Omollo" className="hero-image" />
       <pre><h1>Cliff Omollo</h1></pre>
        <pre><h1>Full-Stack Engineer</h1></pre>
         <p>I build robust, scalable web applications from scratch, turning complex problems into elegant software solutions. Welcome to my digital space.</p>
